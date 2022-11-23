d0d2836b0cd5b65e65a22de7493dd713c48f8751
Imam Mishkat <imammishkat@gmail.com>
2022-11-22 19:29:41 -0500

    new code

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class GameRunner {
+import java.util.Scanner;
+
+public class GameRunner
+{
+    public static void main (String [] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String s = "Welcome to fighting simulator! \nIn this game you get to fight with either the computer or another player!";
+        s += "\n \nThe rules are simple your health bar will have 30 points. \nWhoever has their health drop to 0 first loses.";
+        s += "\nSo lets get started shall we?";
+        String
+
+        System.out.println(s);
+        System.out.print("\nDo you want to play against the computer or player? Press c for computer or p for player: ");
+        String answer = input.nextLine();
+
+
+        if (answer.equals("c"))
+        {
+
+        }
+        else if (answer.equals("p"))
+        {
+
+        }
+        else
+        {
+            System.out.print("That is neither c or p. Do you still wish to continue? (reply with y or n): ");
+            String answer2 = input.nextLine();
+
+        }
+
+
+
+    }
 }
